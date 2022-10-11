b5365299bfd44786649cdafffdf3db4919a1b9cc
tlii0068 <tlii0068@student.monash.edu>
2022-10-11 15:34:11 +0800

    debug for food recipe

--- app.py
+++ app.py
@@ -28,80 +28,80 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-# def drawing_plot():
-#     table_df = pd.read_sql_table(
-#         "dia_year_sex",
-#         con=db.engine,
-#         columns=['id',
-#                  'years',
-#                  'males',
-#                  'females',
-#                  'persons'
-#                  ],
-#     )
-#     years = table_df['years']
-#     males = table_df['males']
-#     females = table_df['females']
-#
-#     m_trend = go.Scatter(
-#         x=years,
-#         y=males,
-#         name='Males',
-#         mode='lines',
-#         line=dict(color='#0099e6')
-#     )
-#
-#     f_trend = go.Scatter(
-#         x=years,
-#         y=females,
-#         name='Females',
-#         mode='lines',
-#         line=dict(color='#99ddff')
-#     )
-#
-#     final_data = [m_trend, f_trend]
-#     fig = go.Figure(final_data).update_xaxes(title_text='Year').update_yaxes(
-#         title_text='Age-standardised per cent').update_layout(
-#         title='Prevalence of type 2 diabetes, by sex, 2000–2020',
-#         font=dict(size=25)).update_layout(
-#             hoverlabel=dict(
-#                 font_size=25,
-#                 font_family="Rockwell"
-#             )
-#     )
-#
-#     py.plot(fig, filename='base_line', auto_open=False, show_link=False)
-#
-#     complication_df = pd.read_sql_table(
-#         "complications_sex",
-#         con=db.engine,
-#         columns=['id',
-#                  'Number_of_Complications',
-#                  'Males',
-#                  'Females',
-#                  'Persons'
-#                  ],
-#     )
-#     num_comp = complication_df['Number_of_Complications']
-#     percentage = complication_df['Persons']
-#     blue_color = ['#e6f2ff', '#80bfff', '#1a8cff',
-#                   '#3366ff', '#005ce6', '#0040ff']
-#     fig2 = go.Figure(data=[go.Pie(labels=num_comp, values=percentage,
-#                                   hole=.6,
-#                                   pull=[0, 0.1, 0.1, 0.1, 0.15, 0.2],
-#                                   marker_colors=blue_color,
-#                                   hoverinfo='skip'
-#                                   )])
-#     fig2.update_layout(
-#         title_text="Number of Complications percentage",
-#         annotations=[dict(text='Age <br> standardised <br> percentage',
-#                           font_size=25, showarrow=False)],
-#         font=dict(size=25)
-#     ).update_layout(legend_title_text='Number of Complications')
-#     py.plot(fig2, filename='pie_c', auto_open=False, show_link=False)
-#
-#
-# drawing_plot()
+def drawing_plot():
+    table_df = pd.read_sql_table(
+        "dia_year_sex",
+        con=db.engine,
+        columns=['id',
+                 'years',
+                 'males',
+                 'females',
+                 'persons'
+                 ],
+    )
+    years = table_df['years']
+    males = table_df['males']
+    females = table_df['females']
+
+    m_trend = go.Scatter(
+        x=years,
+        y=males,
+        name='Males',
+        mode='lines',
+        line=dict(color='#0099e6')
+    )
+
+    f_trend = go.Scatter(
+        x=years,
+        y=females,
+        name='Females',
+        mode='lines',
+        line=dict(color='#99ddff')
+    )
+
+    final_data = [m_trend, f_trend]
+    fig = go.Figure(final_data).update_xaxes(title_text='Year').update_yaxes(
+        title_text='Age-standardised per cent').update_layout(
+        title='Prevalence of type 2 diabetes, by sex, 2000–2020',
+        font=dict(size=25)).update_layout(
+            hoverlabel=dict(
+                font_size=25,
+                font_family="Rockwell"
+            )
+    )
+
+    py.plot(fig, filename='base_line', auto_open=False, show_link=False)
+
+    complication_df = pd.read_sql_table(
+        "complications_sex",
+        con=db.engine,
+        columns=['id',
+                 'Number_of_Complications',
+                 'Males',
+                 'Females',
+                 'Persons'
+                 ],
+    )
+    num_comp = complication_df['Number_of_Complications']
+    percentage = complication_df['Persons']
+    blue_color = ['#e6f2ff', '#80bfff', '#1a8cff',
+                  '#3366ff', '#005ce6', '#0040ff']
+    fig2 = go.Figure(data=[go.Pie(labels=num_comp, values=percentage,
+                                  hole=.6,
+                                  pull=[0, 0.1, 0.1, 0.1, 0.15, 0.2],
+                                  marker_colors=blue_color,
+                                  hoverinfo='skip'
+                                  )])
+    fig2.update_layout(
+        title_text="Number of Complications percentage",
+        annotations=[dict(text='Age <br> standardised <br> percentage',
+                          font_size=25, showarrow=False)],
+        font=dict(size=25)
+    ).update_layout(legend_title_text='Number of Complications')
+    py.plot(fig2, filename='pie_c', auto_open=False, show_link=False)
+
+
+drawing_plot()
 
 # FOOD RECIPE
 food_df = pd.read_sql_table(
@@ -510,8 +510,8 @@ class Recipe:
 
 @app.route("/add", methods=["POST"], strict_slashes=False)
 def add_articles():
-    height = 160#float(request.json['height'])
-    weight = 55#float(request.json['weight'])
+    height = float(request.json['height'])
+    weight = float(request.json['weight'])
 
     bmi = weight/((height/100)**2)
 
@@ -548,7 +548,7 @@ def scan_jpg():
 #     单选 [Chicken, Beef, Fish, Pork, Seafood]
 #
 #     search= request.json["keyword"]
-@app.route("/recipe")
+@app.route("/recipe", methods=["POST"], strict_slashes=False)
 def all_tag_food_recipe():
     dict_name_list = ['NO TAG', 'Low Fat', 'Low GI', 'Low Fat-Low GI', 'Halal', 'Low Fat-Halal', 'Low GI-Halal',
                       'Low Fat-Low GI-Halal', 'Low Carbohydrate', 'Low Fat-Low Carbohydrate', 'Low GI-Low Carbohydrate',
